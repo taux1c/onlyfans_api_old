34074d93c5621d65019aef63dd0319f768e6c634
qpidcock <24677318+qpidcock@users.noreply.github.com>
2022-10-01 14:04:31 -0400

    Completed profile.py for now.
    I will need to add methods later to allow for download functionality but it would work for now.

--- cogs/profile.py
+++ cogs/profile.py
@@ -10,7 +10,7 @@ class Profile:
     def __init__(self,auth_file,config_file):
         self.config = cogs.config.Config(config_file)   # This is where the config file is passed to the profile class and as a result the scraper.
         self.active_subscriptions = []   # This is a list of all the active subscriptions for this profile.
-        self.messaged_models = self.    # This is a list of all the models that have been messaged by this profile.
+        self.messaged_models = []   # This is a list of all the models that have been messaged by this profile.
         self.typed_usernames = []     # This is a list of all the usernames that have been typed by this profile. Each name stored as a string.
         self.auth = cogs.auth.Auth(auth_file)   # This is the auth object that will be used to authenticate the profile.
         self.config = cogs.config.Config(config_file)   # This is the config object that will be used to configure the scraper for the profile.
