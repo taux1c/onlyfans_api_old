f0a93286f86457bfe222272f470d4a28a26dbd9c
qpidcock <24677318+qpidcock@users.noreply.github.com>
2022-10-01 14:02:58 -0400

    Completed profile.py for now.
    I will need to add methods later to allow for download functionality but it would work for now.

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,6 +1,6 @@
 import constants
 import cogs.auth
-
+import cogs.config
 
 # This is the class that will be used to store  the scrapers profile data. Scrapers will import this class and call methods of the profile.
 # For example, jackmehauf2382.scrape('username') or jackmehauf2382.scrape_all() each command will take an argument such as 'photos', 'videos', 'audios'.
@@ -8,15 +8,16 @@ import cogs.auth
 
 class Profile:
     def __init__(self,config_file):
-        self.active_subscriptions = []
-        self.messaged_models = []
-        self.typed_usernames = []
-        self.auth = cogs.auth.Auth(auth_file)
-        self.config = cogs.config.Config(config_file)
-        self.messages = []
-        self.archived_messages = []
-        self.typed_usernames_models = []
-        self.bad_usernames = []
+        self.config = cogs.config.Config(config_file)   # This is where the config file is passed to the profile class and as a result the scraper.
+        self.active_subscriptions = []   # This is a list of all the active subscriptions for this profile.
+        self.messaged_models = self.    # This is a list of all the models that have been messaged by this profile.
+        self.typed_usernames = []     # This is a list of all the usernames that have been typed by this profile. Each name stored as a string.
+        self.auth = cogs.auth.Auth(auth_file)   # This is the auth object that will be used to authenticate the profile.
+        self.config = cogs.config.Config(config_file)   # This is the config object that will be used to configure the scraper for the profile.
+        self.messages = []  # This is a list of all the messages that have been sent / received by this profile.
+        self.archived_messages = [] # This is a list of all the archived messages that have been sent / received by this profile.
+        self.typed_usernames_models = []    # This is a list of all the models that have been typed by this profile in object form if they were able to be constructed.
+        self.bad_usernames = []     # This is a list of all the usernames that have been typed by this profile that were not able to be constructed into a model object.
 
 
 
